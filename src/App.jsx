ef80c08888da5b594965386ed63e095e47088d8f
Sathwik <sathwikv2005@gmail.com>
2025-04-28 16:59:07 +0530

    add: page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
+import Error404 from './pages/Error404'
+import Cart from './pages/Cart'
+import NavBarLayout from './layouts/navBarLayout.jsx'
+import Home from './pages/Home.jsx'
 
 export default function App() {
-	return <div>Hello!</div>
+	return (
+		<Routes>
+			<Route path="/" element={<NavBarLayout />}>
+				<Route index element={<Home />} />
+				<Route path="/menu" element={<Home />} />
+				<Route path="/cart" element={<Cart />} />
+				<Route path="*" element={<Error404 />} />
+			</Route>
+		</Routes>
+	)
 }
