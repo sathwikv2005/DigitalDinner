ec392215dbb051b41efbdd69c33afd35aa244a76
Sathwik <sathwikv2005@gmail.com>
2025-04-28 17:01:14 +0530

    add: navbar

---
+++ src/components/navBar.jsx
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom'
+import { BsCart3 } from 'react-icons/bs'
+import React from 'react'
+
+export default function NavBar() {
+	return (
+		<>
+			<div className="navbar">
+				<div className="navbar--title">
+					<Link className="navbar--title--link sour-gummy" to="/">
+						Digital Diner
+					</Link>
+				</div>
+				<div className="nav--right-items inter">
+					<Link to="/menu">Menu</Link>
+					<Link to="/cart">Cart</Link>
+					<Link to="/cart" className="navbar--cart--a">
+						<BsCart3 className="navbar--cart--icon" />
+					</Link>
+				</div>
+			</div>
+		</>
+	)
+}
