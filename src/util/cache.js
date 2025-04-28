8f87b6cdb21fa81d863eb2a4c48d85895cb8851a
Sathwik <sathwikv2005@gmail.com>
2025-04-28 19:44:49 +0530

    update cache logic

--- src/util/cache.js
+++ src/util/cache.js
@@ -1,35 +1,26 @@
 export function addCache(name, obj) {
 	const cacheObj = {
-		name,
 		obj,
 		createdAt: Date.now(),
 	}
 
-	const cache = JSON.parse(localStorage.getItem('cache')) || []
-	cache.push(cacheObj)
-	localStorage.setItem('cache', JSON.stringify(cache))
+	localStorage.setItem(name, JSON.stringify(cacheObj))
+	return true
 }
 
-export async function getCache(name) {
-	const cache = JSON.parse(localStorage.getItem('cache')) || []
-
-	const cacheObj = cache.find((x) => x.name === name)
+export function getCache(name) {
+	const cacheObj = localStorage.getItem(name)
 	if (!cacheObj) return null
-	return cacheObj
+
+	return JSON.parse(cacheObj)
 }
 
 export function updateCache(name, newObj) {
-	const cache = JSON.parse(localStorage.getItem('cache')) || []
-
-	const index = cache.findIndex((x) => x.name === name)
-	if (index === -1) return false
-
-	cache[index] = {
-		name,
+	const cacheObj = {
 		obj: newObj,
 		createdAt: Date.now(),
 	}
 
-	localStorage.setItem('cache', JSON.stringify(cache))
+	localStorage.setItem(name, JSON.stringify(cacheObj))
 	return true
 }
