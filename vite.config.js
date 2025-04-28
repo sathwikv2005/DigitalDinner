55696e89ceaa457735df1edc85c0c72a3459a450
Sathwik <sathwikv2005@gmail.com>
2025-04-28 18:32:02 +0530

    config proxy for development

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://127.0.0.1:6700',
+				changeOrigin: true,
+				secure: false,
+			},
+		},
+	},
 })
